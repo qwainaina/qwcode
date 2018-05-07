df6c1633d97f6f5608f3c8208f8aa96322fd1cc1
qwainaina <33621505+qwainaina@users.noreply.github.com>
2018-05-07 15:35:24 +0300

    rsa exercise code

---
+++ cpp/rsa.cpp
@@ -0,0 +1,95 @@
+//Control Structures
+#include <iostream>
+using namespace std;
+int main ()
+
+// If statement
+//Formart: if (condition) statement
+{
+
+/*
+int a = 6;
+
+if (a> 4) 
+    {
+        cout << a;
+    }
+    
+
+
+// Else statement
+
+// Formart else statement
+
+if (a > 0)
+{
+    cout << " a is Greater" <<endl;
+}
+else {
+    cout << " A is lesser" << endl;
+}
+*/
+
+//int a,b,c;
+  
+  /*  
+  int  a=5;
+ 
+ if(a>3)
+ {
+     cout<<"a is bigger than 3"<<endl;
+ }
+ 
+ else 
+ { 
+  cout<< "a is smaller than 3"<<endl;
+ }
+ */
+ 
+ //else if 
+ // Formart else if (condition) statement
+ 
+/* 
+ int a = 0;
+ if (a > 1) {
+     cout << "a is greater 0" <<endl;
+ }
+ else if (a == 0) {
+     cout << "a is equal to 0" << endl;
+ }
+ else 
+ {
+     cout << "a is less than 0" << endl;
+ }
+    
+}
+
+*/
+
+/* write a programe thats asks a user to input their age
+and if the user is younger than 18 is not allowed, only 18 or older than 18 allowed */
+
+// Club entrance App
+
+int age;
+
+cout <<"How old are you?"<<endl;
+cin >> age;
+
+if (age < 18)
+{
+
+    cout<<"Sorry! you are too young to enter the Club."<<endl;
+}
+
+else if (age >= 18)
+{
+    cout<<"You may enter the club!"<<endl;    
+}
+    
+}
+
+
+
+
+
